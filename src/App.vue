9d73b2a48a577a3ce389a4c8f87ccd004233c2ea
Sarah Paz <sarah.paz@gmail.com>
2020-08-15 16:45:40 -0400

    add submit button styling

--- src/App.vue
+++ src/App.vue
@@ -58,4 +58,16 @@ body {
   font-family: Arial, Helvetica, sans-serif;
   line-height: 1.4;
 }
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+.btn:hover {
+  background: #666;
+}
 </style>
